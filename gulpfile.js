3b217db802843aed15a5e1eeea8e0d426245fe99
Paul Irwin <paulirwin@gmail.com>
2020-02-08 12:03:12 -0700

    Update gulp/babel to fix broken build

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,9 @@ var uglify = require("gulp-uglify");
 gulp.task("default", function () {
     return gulp.src("src/**/*.es6")
         .pipe(sourcemaps.init())
-        .pipe(babel())
+        .pipe(babel({
+            presets: ["@babel/env"]
+        }))
         .pipe(concat("maidenhead.js"))
         .pipe(uglify())
         .pipe(sourcemaps.write("."))
